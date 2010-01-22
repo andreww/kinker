3ac244ad8d1c23a2014c68b141a17f13cda86f65
Andrew Walker <andreww@boa.niees.group.cam.ac.uk>
2010-01-22 14:32:19 +0000

    Moves local changes to kinker into exptplot

--- exptplot.py
+++ exptplot.py
@@ -7,14 +7,18 @@ FILE = open('MgO_experiments.dat', 'r')
 T,tau = loadtxt(FILE, unpack=True)
 FILE.close()
 
+
 plt.figure(1)
 plt.plot(T,tau,'o')
 plt.xlabel('T')
 plt.ylabel('tau')
 plt.title('crss')
 
-T_n = T/750.0
-tau_n = (tau-10.0)/55.0
+T_n = T/500.0
+# Don't know why I need to / 5000
+# but 9.0 is tau at Tcrit (removes background
+# from this dataset).
+tau_n = (tau-9.0)/5000
 plt.figure(2)
 plt.plot(tau_n,T_n,'o')
 plt.xlabel('tau_n')
