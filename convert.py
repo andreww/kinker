a3a82ac518d1881c274bb651fb121ea7b5baf338
Andrew <a.walker@ucl.ac.uk>
2010-02-26 15:17:15 +0000

    Clean up kinker for GoGo talk
    (Units and use fitted func cos it's smoother).

---
+++ convert.py
@@ -0,0 +1,13 @@
+
+
+def m_2_Ang(x):
+    return(x/1E-10)
+
+def Pa_2_MPa(x):
+    return(x/1E6)
+
+def J_2_eV(x):
+    return(x/1.60217646E-19)
+
+def Jpm_2_eVpAng(x):
+    return(J_2_eV(x)*1E-10)
--- kinker.py
+++ kinker.py
@@ -4,6 +4,8 @@ from numpy import *
 import matplotlib.pyplot as plt
 from scipy import interpolate
 from peierls import kp_energy, num_integ, kinker
+import peierls_pot as pot
+from convert import *
 
 def load_data (filename):
     """Load data from filename, if None load example data."""
@@ -43,35 +45,60 @@ Na = 6.022E23 # Avagadro's number
 (xfine, yfine, sigma_p, zdiff, u_0, u_max, H_kp, Un, \
  zdiff_kp, yderfine, sigma_b, sigma_p_index, kink_energy2) = kinker(x,y, maxx=xmax_input)
 
+# Now do it again using a function...
+
+func = pot.choose_func()
+
+# Fit this data to a sin function
+print "Fitting initial potential to sin function"
+#p0 = [2.4E-10, 2.4E-10, 1, 2, 1, 2, 3, 4, 1, 2]
+p0 = [2.4, 0.001, 0.001, 2.0, 2.0, 2.0]
+opt_p = pot.fit_pot(p0, x, y, max(x), func)
+
+# Initial solution with this function
+(xfine_init, yfine, sigma_p, zdiff_init, u_0_init, u_max, H_kp, Un_init, \
+ zdiff_kp, yderfine_init, sigma_b_init, sigma_p_index, kink_energy2_init) = kinker(x,y,G=60E9,method=func,params=opt_p)
+
+
+# Load experimental data
+exptdata = raw_input("Expt data (input is basname.dat): ")
+(T_n,expt_tau) = load_data(exptdata)
+# Convert from MPa to Pa and subtract tau_crit (15 MPa, T_crit is 500 K, handled in error func.)
+tau_n = expt_tau*1E6
+t_crit = 650
+tau_crit = 10.0E6
+
 # Plot data 
 plt.figure(1)
-plt.plot(x,y,'o',xfine,yfine,'-')
-plt.ylabel('U')
-plt.xlabel('u')
-plt.title('U(u)')
+plt.plot(m_2_Ang(x),Jpm_2_eVpAng(y),'o',m_2_Ang(xfine),Jpm_2_eVpAng(func(xfine,opt_p, max(x))),'-')
+plt.ylabel('E (eV/Ang)')
+plt.xlabel('x (Ang)')
+plt.title('E(x)')
 
 plt.figure(2)
-plt.plot(xfine[1:],zdiff,'-')
-plt.ylabel('z-z0')
-plt.xlabel('u')
+plt.plot(m_2_Ang(xfine[1:]),m_2_Ang(zdiff),'-')
+plt.ylabel('z (Ang)')
+plt.xlabel('x (Ang)')
 plt.title('Geometrical kink shape')
 
 plt.figure(3)
-plt.plot(xfine,yderfine,'--',u_0,sigma_b,'o',u_max,sigma_b,'+',array([xfine[sigma_p_index]]),array([sigma_p]),'x')
+plt.plot(m_2_Ang(xfine),Pa_2_MPa(yderfine),'--',m_2_Ang(u_0_init),Pa_2_MPa(sigma_b_init),'o') #,u_max,sigma_b,'+') # ,array([xfine[sigma_p_index]]),array([sigma_p]),'x')
 plt.title('Derivative of potential')
-plt.ylabel('dU/du')
-plt.xlabel('u')
+plt.ylabel('dE/dx (MPa)')
+plt.xlabel('x (Ang)')
 
 plt.figure(4)
-plt.plot((sigma_b/5.0E-10),(Un*Na/1000),'--')
-plt.title('Critical stress / kink energy')
-plt.xlabel('Tau* - Pa')
-plt.ylabel('Un - kJ/mol')
+plt.plot((sigma_b_init/sigma_p),(Un_init/(kink_energy2_init*2.0)),'--',(sigma_b_init/sigma_p),(Un_init/(kink_energy2_init*2.0)),'o')
+plt.title('CRSS / kink energy (relative)')
+plt.xlabel('sigma*/sigma_p')
+plt.ylabel('Un/2Uk')
 
 plt.figure(5)
-plt.plot((sigma_b/sigma_p),(Un/(kink_energy2*2.0)),'--',(sigma_b/sigma_p),(Un/(kink_energy2*2.0)),'o')
-plt.title('Critical stress / kink energy')
-plt.xlabel('Tau*/sigma_p')
-plt.ylabel('Un/2Uk')
+plt.plot(((Un_init/(kink_energy2_init*2.0))*t_crit),Pa_2_MPa(sigma_b_init+tau_crit),'--',T_n,Pa_2_MPa(tau_n),'o',)
+plt.legend(('model', 'expt data'))
+plt.title('CRSS / kink energy (absolute)')
+plt.ylabel('CRSS (MPa)')
+plt.xlabel('T (K)')
+
 
 plt.show()
